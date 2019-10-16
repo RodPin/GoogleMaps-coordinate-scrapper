83a98f3e2897c627e6e555be244948a6159a61d7
RODRIGO <prodrigo@poli.ufrj.br>
2019-10-16 19:20:47 -0300

    search refactor

--- main.py
+++ main.py
@@ -9,16 +9,32 @@ chrome_options = DriverConfig.get_options()
 driver = webdriver.Chrome(options=chrome_options, executable_path=DriverConfig.get_system_driver())
 
 driver.get("https://www.google.com.br/maps")
+
+ZoomAdjust = {
+    '4':[0.0, 0.0],
+    '6':[0.0 ,0.0],
+    '9':[0.0, 0.0],
+    '10':[0.0, 0.0],
+    '11':[0.0, 0.0],
+    '12':[0.0, 0.0],
+    '14':[0.0, 0.0],
+    '15':[0.0, 0.0],
+    '16':[0.0, 0.0],
+    '17':[0.0001271, 0.0021159],
+    '18':[0.0,0.0],
+    '19':[0.0,0.0]
+}
+
 def searchOnMaps(endereco):
     digitarEndereco = driver.find_element_by_id("searchboxinput")
     digitarEndereco.clear()
     digitarEndereco.send_keys(endereco)
     digitarEndereco.send_keys(Keys.ENTER)
     sleep(4)
-    # clickar=driver.find_element_by_class_name("section-result-header")
-    # if clickar:
-    #     clickar.click()
-    # sleep(2)
+    elements = driver.find_elements_by_class_name('section-result')
+    if elements is not None and len(elements) > 0:
+        elements[0].click()
+        sleep(3)
     url = driver.current_url
 
     def find_between( s, first, last ):
@@ -33,82 +49,49 @@ def searchOnMaps(endereco):
     arrayLatLongZoom=latLongZoom.split(",")
     latitude=arrayLatLongZoom[0]
     longitude=arrayLatLongZoom[1]
-    # print(float(latitude)-float(0.0001271))
-    # print(float(longitude) + float(0.0021159))
+    zoom=arrayLatLongZoom[2][:-1]
     
-    if arrayLatLongZoom[2][1]!='z':
-        zoom=arrayLatLongZoom[2][0]+arrayLatLongZoom[2][1]
-      
-    else:
-        zoom=arrayLatLongZoom[2][0]
-
-    if int(zoom)>15:
-        latitude=float(latitude)-float(0.0001271)
-        longitude=float(longitude) + float(0.0021159)
+    if zoom in ZoomAdjust and int(zoom) > 15:
+        latitude=float(latitude)-float(ZoomAdjust[zoom][0])
+        longitude=float(longitude) + float(ZoomAdjust[zoom][1])
         print('LatitudeLongitude: '+ str(latitude)+' '+ str(longitude)+ '     Zoom: '+zoom)
         return [str(latitude),str(longitude),zoom]
     else:
         print('LatitudeLongitude: '+ str(latitude)+' '+ str(longitude) + '    Zoom:'+zoom)
         return ['ERRO','erro',zoom]
 
-# sleep(10)
-# driver.quit()
-latLong=[]
-ocurrencyArray=[]
-def treatList(i,count):
-    ## SEARCH ON MAP
-    ocurrencyArray
-
-    endereco=f[i][8]+' '+f[i][3]+' '+f[i][4]+' '+f[i][5]
-    latLong=searchOnMaps(endereco) 
-    if count>0:
-        for x in range(count+1):
-            # f[i-x][9]=str(count) ##USE RESULTS
-            # f[i-x][10]=str(count) ##USE RESULTS
-            f[i-x][9]=latLong[0]
-            f[i-x][10]=latLong[1]
-            ocurrencyArray.append(latLong[2])
-
-    else:
-        f[i][9]=latLong[0]
-        f[i][10]=latLong[1]
-        ocurrencyArray.append(latLong[2])
-
-
-
-
-f= open("./adress","r").readlines()
-
-# lat='5.3171555'
-# longs='-41.5828389'   
-# print(f[1])
-# f[1]=f[1].split(',')
-# f[1][9]=lat   
-# f[1][10]=longs
-pool=4
-for x in range(pool):
-    f[x]=f[x].split(';')
-
-count=0
-
-for i in range(pool):
-    if f[i][3] == f[i+1][3] and f[i][2] == f[i+1][2] and f[i][4] == f[i+1][4] and f[i][5] == f[i+1][5] and f[i][8] == f[i+1][8]:
-        count=count+1
-    else:
-        #Pesquisa coordenadas
-        #i, contador,
-        # print(f[i][3])
-        treatList(i,count)
-        count=0
-    
-errorCount=0
-for i in range(pool):
-    if f[i][10]=='erro':
-        errorCount=errorCount+1
-    virgula=','
-    f[i]=virgula.join(f[i])
-    # print(f[i])
-print('ERRORS:')
-print(errorCount)
-print('ZOOMS:')
-print(collections.Counter(ocurrencyArray))
+if __name__ == '__main__':
+    f = open("./adress","r")
+    content = f.readlines()
+    f.close()
+    organs = {}
+    addresses = set()
+    for line in content:
+        data = line.split(';')
+        address = " , ".join([data[2] + ' ' + data[3], data[4], data[5], data[7], data[8]])
+        addresses.add(address)
+        if address in organs:
+            organs[address] += [line]
+        else:
+            organs[address] = [line]
+    coordinates = []
+    zoom_count = {}
+    new_lines = ""
+    for address in addresses:
+        lat_lon = searchOnMaps(address)
+        coordinates.append(lat_lon)
+        if lat_lon[-1] in zoom_count:
+            zoom_count[lat_lon[-1]] += 1
+        else:
+            zoom_count[lat_lon[-1]] = 1
+        for i in range(len(organs[address])):
+            address_list = organs[address] 
+            string = address_list[i]
+            address_list[i] = string.rstrip('\n')[:-1] + lat_lon[0] + ";" + lat_lon[1]
+            organs[address] = address_list
+            new_lines += organs[address][i] + '\n'
+    new_file = open('./adress_lat_lon.txt', 'w')
+    new_file.write(new_lines)
+    new_file.close()             
+    print(zoom_count)
+    driver.quit()
